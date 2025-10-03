0707ec8f98d6fa9ccde3ea837b706066311d8278
Alessandro Lorenz <alessandro.lorenz@compasso.com>
2025-10-03 08:50:11 -0300

    refactor: Remove SmsSignup component from SignupPage

--- macys-presentation/src/views/SignupPage.vue
+++ macys-presentation/src/views/SignupPage.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="container-center">
     <SignupForm />
-    <SmsSignup />
   </div>
 </template>
 
 <script>
 import SignupForm from '../components/SignupForm/SignupForm.vue';
-import SmsSignup from '../components/SmsSignup/SmsSignup.vue';
 
 export default {
   name: 'SignupPage',
